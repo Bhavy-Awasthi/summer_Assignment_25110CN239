3ae79254e8ec8a8d8707876d2c7ea5f6fb20ac47
Bhavy-Awasthi <bhavyawasthi4974stella@gmail.com>
2026-05-25 20:56:18 +0530

    C

--- sum_natural.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main() {
-    int n, sum = 0;
-    printf("Enter the number of natural numbers to be added : ");
-    scanf("%d", &n);
-    sum=(n*(n+1))/2;
-    printf("The sum of first %d natural numbers is : %d", n, sum);
-    return 0;
-}
