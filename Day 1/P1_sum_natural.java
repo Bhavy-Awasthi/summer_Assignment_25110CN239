62ef06698a5052e2a3e09a45f46ec75fa3f39dfa
Bhavy-Awasthi <bhavyawasthi4974stella@gmail.com>
2026-05-26 04:13:54 +0530

    J

--- Day 1/P1_sum_natural.java
+++ Day 1/P1_sum_natural.java
@@ -5,7 +5,6 @@ public class P1_sum_natural {
         int n, sum = 0;
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number to find the sum of natural numbers : ");
-
         n = sc.nextInt();
         sum = (n * (n + 1)) / 2;
         System.out.println("The sum of first " + n + " natural numbers is : " + sum);
