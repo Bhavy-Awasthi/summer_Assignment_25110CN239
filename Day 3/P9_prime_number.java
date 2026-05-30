80c367e7349f3d5988870cc268a79aa9404e5f57
Bhavy-Awasthi <bhavyawasthi4974stella@gmail.com>
2026-05-30 22:58:27 +0530

    Assignment codes added

--- Day 3/P9_prime_number.java
+++ Day 3/P9_prime_number.java
@@ -4,7 +4,7 @@ class P9_prime_number {
     public static void main() {
         int n, i, count = 0;
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a number: ");
+        System.out.println("Enter a number: ");
         n = sc.nextInt();
         for (i = 1; i <= n; i++) {
             if (n % i == 0) {
